f78d150cc1c0861dc98e33aab92a2157ecf77083
varunM43 <varunmanukonda43@gmail.com>
2021-08-31 13:04:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/week4/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.week4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        supportFragmentManager.beginTransaction().replace(R.id.fragment_layout,regform()).commit()
+    }
+
+
+
+}
